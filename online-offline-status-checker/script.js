4a070317fa9577fa6f87a9eb80ec6d50d48daefe
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2024-03-12 21:19:41 +0530

    Add files via upload

--- online-offline-status-checker/script.js
+++ online-offline-status-checker/script.js
@@ -2,10 +2,10 @@ function updateOnlineStatus() {
     const statusElement = document.getElementById('status');
     if (navigator.onLine) {
       statusElement.textContent = 'You are currently online.';
-      statusElement.style.color = '#28a745'; // Green color for online
+      statusElement.style.color = '#28a745';
     } else {
       statusElement.textContent = 'You are currently offline.';
-      statusElement.style.color = '#dc3545'; // Red color for offline
+      statusElement.style.color = '#dc3545'; 
     }
   }
   
